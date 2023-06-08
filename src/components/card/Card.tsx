2fa0d392f7364c38dce579a87fdf8ea68b5d46f0
Philip Söderbom <philip.m.soderbom@gmail.com>
2023-06-08 19:59:44 +0200

    changed colors

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -14,7 +14,7 @@ export default function Card(props: Props) {
     return (
         <Link href={`${fixString(props.text)}`} className="card m-4 p-6 bg-white rounded-2xl">
             <div>
-                <h2 className="text-2xl">{props.text}</h2>
+                <h2 className="text-2xl text-emerald-500">{props.text}</h2>
                 <p>Additional text</p>
             </div>
         </Link>
