ff0d97fea3b5e7401d3c47785b64132b1d70aac6
Philip Söderbom <philip.m.soderbom@gmail.com>
2023-06-08 17:00:45 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Card from "@/components/card/Card";
-import Link from "next/link";
 
 const cardTitles = ['Todo List', 'Notes', 'Logs', 'Philip']
 
 export default function Home() {
   return (
     <div className="grid-main">
-      <h1 className="text-center text-6xl my-8">Dashboard</h1>
+      <h1 className="text-center text-6xl my-8">Philip's Dashboard</h1>
       <div className="cards grid grid-cols-2">
         {cardTitles.map((cardTitle, i) => (
           <Card text={cardTitle}></Card>
