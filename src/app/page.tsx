5727da79dbcc5ae1d73aa022556b454bee3cc293
Philip Söderbom <philip.m.soderbom@gmail.com>
2023-06-08 22:47:36 +0200

    SASS stable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="grid-main">
       <h1 className="text-center text-6xl my-8">Philip's Dashboard</h1>
-      <div className="cards grid grid-cols-2">
+      <div className="cards grid grid-cols-3">
         {cardTitles.map((cardTitle, i) => (
           <Card key={i} text={cardTitle}></Card>
         ))}
