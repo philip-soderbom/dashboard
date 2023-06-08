1c2e392584e8de6cb64dab1969ab94f353830897
Philip Söderbom <philip.m.soderbom@gmail.com>
2023-06-09 00:22:56 +0200

    home layout done

---
+++ src/components/calendarContainer/CalendarContainer.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from "../ui/calendar";
+
+export default function CalendarContainer() {
+    return (
+        <div className="bg-red-300 m-4">
+            <Calendar></Calendar>
+        </div>
+
+    )
+}
---
+++ src/components/imageContainer/ImageContainer.tsx
@@ -0,0 +1,9 @@
+const imgurl2 = "../../assets/imgs/vinijr.png"
+
+export default function ImageContainer() {
+    return (
+        <div className="bg-blue-300 m-4 p-6">
+            <img src={imgurl2} alt="real image" />
+        </div>
+    )
+}
