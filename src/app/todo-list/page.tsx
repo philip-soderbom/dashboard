87046507eb0953403443ff460a7baafe0eafaa65
Philip Söderbom <philip.m.soderbom@gmail.com>
2023-06-10 15:50:03 +0200

    TodoItem added

--- src/app/todo-list/page.tsx
+++ src/app/todo-list/page.tsx
@@ -1,5 +1,34 @@
+import TodoItem from "@/components/todoItem/TodoItem";
+import { prisma } from "@/db";
+import Link from "next/link";
 import React from "react";
 
-export default function Page() {
-    return <h1>Todo List</h1>
+
+function getTodos() {
+    return prisma.todo.findMany()
+}
+
+export default async function Page() {
+
+    const todos = await getTodos()
+
+    return (
+        <>
+            <div className="mt-24 flex justify-between item-center mb-4">
+                <h1 className="text-3xl">Todos</h1>
+                <Link
+                    className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+                  hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+                    href="/new"
+                >New</Link>
+            </div>
+
+            <ul className="pl-4">
+                {todos.map((todo) => (
+                    <TodoItem key={todo.id} {...todo}></TodoItem>
+                ))}
+            </ul>
+        </>
+
+    )
 }
---
+++ src/components/todoItem/TodoItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type TodoItemProps = {
+    id: string,
+    title: string,
+    complete: Boolean
+}
+
+export default function TodoItem({ id, title, complete }: TodoItemProps) {
+    return (
+        <li className='flex gap-1 items-center'>
+            <input id={id} type="checkbox" className='cursor-pointer peer' />
+            <label htmlFor={id} className=' cursor-pointer peer-checked:line-through peer-checked:text-slate-400'>{title}</label>
+        </li>
+    )
+}
